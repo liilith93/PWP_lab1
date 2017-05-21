392b410b9e356d6a56a500eb5d2847a06cfdccf3
Urszula Mołdysz <Urszula Modysz>
2017-05-21 21:43:06 +0200

    trzeci commit opcja włączona

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+#include "LaborkaConfig.h"
+
+#ifdef USE_TRIGONOMETRY_DEGREE
 #include "trygonometria.h"
+double result = cos_degree(45.0);
+#else
+#include <math.h>
+#define M_PI 3.14159265358979323846
+double result = cos(M_PI / 4.0);
+#endif
 
 int main() {
 double sinus = sin_degree(90.0);
